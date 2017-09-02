856a3e463300b61906dc93bff675f2bd6ad850e0
guo <1599485508@qq.com>
2017-09-02 22:50:11 +0800

    lock versions

--- node_modules/art-template-loader/example/filter/index.js
+++
@@ -1,9 +0,0 @@
-require('./template-filter');
-const render = require('./index.art');
-const data = {
-    time: 1493791254254
-};
-const html = render(data);
-console.log(html);
-
-module.exports = render;
--- node_modules/art-template-loader/example/layout/index.js
+++
@@ -1,8 +0,0 @@
-const render = require('./index.art');
-const data = {
-    title: 'My Page'
-};
-const html = render(data);
-console.log(html);
-
-module.exports = render;
